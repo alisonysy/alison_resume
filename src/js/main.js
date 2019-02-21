7100d0b8f37f66d1e18854a3b688944b8b7a2b02
alisonys <qwen735@aucklanduni.ac.nz>
2019-02-21 14:12:33 +0800

    finish header

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+{
+  data={
+    uoa:{
+     uni:'奥克兰大学',
+     major:'信息管理和市场营销(双专业)',
+     gpa:'7.8/9.0',
+     time:'2016.02 - 2019.05' 
+    },
+    xmu:{
+      uni:'厦门大学',
+      major:'法语语言文学',
+      gpa:'3.6/4.0',
+      time:'2013.09 - 2015.12' 
+    }
+  }
+  let vm = new Vue({
+    el:'.educationDetails',
+    data:data
+  })
+}
